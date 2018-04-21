28c53849e63637668ea30fb0ae1ec97082f25481
Efosa Uwa-Omede <euwaomed@nyit.edu>
2018-04-20 22:18:41 -0400

    Connect4 Done. Feel free to play around with

--- src/connect4/Connect4App.java
+++ src/connect4/Connect4App.java
@@ -1,6 +1,8 @@
 package connect4;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.effect.Light;
@@ -11,9 +13,14 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 //Author: Efosa Uwa-Omede
@@ -25,13 +32,18 @@ public class Connect4App extends Application {
     private static final int COLUMNS = 7;
     private static final int ROWS = 6;
 
-    private Parent createContent(){
+    private boolean redMove = true;
+    private Disc[][] grid = new Disc[COLUMNS][ROWS];
+
+    private Pane discRoot = new Pane();
+
+    private Parent createContent() {
         Pane root = new Pane();
+        root.getChildren().add(discRoot);
 
         Shape gridShape = makeGrid();
         root.getChildren().add(gridShape);
         root.getChildren().addAll(makeColumns());
-
 
         return root;
     }
@@ -44,12 +56,13 @@ public class Connect4App extends Application {
                 Circle circle = new Circle(TILE_SIZE / 2);
                 circle.setCenterX(TILE_SIZE / 2);
                 circle.setCenterY(TILE_SIZE / 2);
-                circle.setTranslateX(x * (TILE_SIZE + 5) + TILE_SIZE /3);
-                circle.setTranslateY(y * (TILE_SIZE + 5) + TILE_SIZE /3);
+                circle.setTranslateX(x * (TILE_SIZE + 5) + TILE_SIZE / 3);
+                circle.setTranslateY(y * (TILE_SIZE + 5) + TILE_SIZE / 3);
 
                 shape = Shape.subtract(shape, circle);
             }
         }
+
         Light.Distant light = new Light.Distant();
         light.setAzimuth(45.0);
         light.setElevation(30.0);
@@ -58,8 +71,8 @@ public class Connect4App extends Application {
         lighting.setLight(light);
         lighting.setSurfaceScale(5.0);
 
-        shape. setFill(Color.BLUE);
-        shape. setEffect(lighting);
+        shape.setFill(Color.BLUE);
+        shape.setEffect(lighting);
 
         return shape;
     }
@@ -67,19 +80,118 @@ public class Connect4App extends Application {
     private List<Rectangle> makeColumns() {
         List<Rectangle> list = new ArrayList<>();
 
-        for (int x = 0; x < COLUMNS; x++){
-            Rectangle rect = new Rectangle(TILE_SIZE , (ROWS + 1)* TILE_SIZE);
-            rect.setTranslateX(x * (TILE_SIZE + 5) + TILE_SIZE /3);
+        for (int x = 0; x < COLUMNS; x++) {
+            Rectangle rect = new Rectangle(TILE_SIZE, (ROWS + 1) * TILE_SIZE);
+            rect.setTranslateX(x * (TILE_SIZE + 5) + TILE_SIZE / 3);
             rect.setFill(Color.TRANSPARENT);
 
             rect.setOnMouseEntered(e -> rect.setFill(Color.rgb(200, 200, 50, 0.3)));
             rect.setOnMouseExited(e -> rect.setFill(Color.TRANSPARENT));
 
+            final int column = x;
+            rect.setOnMouseClicked(e -> placeDisc(new Disc(redMove), column));
+
             list.add(rect);
         }
 
         return list;
+    }
 
+    private void placeDisc(Disc disc, int column) {
+        int row = ROWS - 1;
+        do {
+            if (!getDisc(column, row).isPresent())
+                break;
+
+            row--;
+        } while (row >= 0);
+
+        if (row < 0)
+            return;
+
+        grid[column][row] = disc;
+        discRoot.getChildren().add(disc);
+        disc.setTranslateX(column * (TILE_SIZE + 5) + TILE_SIZE / 3);
+
+        final int currentRow = row;
+
+        TranslateTransition animation = new TranslateTransition(Duration.seconds(0.125), disc);
+        animation.setToY(row * (TILE_SIZE + 5) + TILE_SIZE / 3);
+        animation.setOnFinished(e -> {
+            if (gameEnded(column, currentRow)) {
+                gameOver();
+            }
+
+            redMove = !redMove;
+        });
+        animation.play();
+    }
+
+    private boolean gameEnded(int column, int row) {
+        List<Point2D> vertical = IntStream.rangeClosed(row - 3, row + 3)
+                .mapToObj(r -> new Point2D(column, r))
+                .collect(Collectors.toList());
+
+        List<Point2D> horizontal = IntStream.rangeClosed(column - 3, column + 3)
+                .mapToObj(c -> new Point2D(c, row))
+                .collect(Collectors.toList());
+
+        Point2D topLeft = new Point2D(column - 3, row - 3);
+        List<Point2D> diagonal1 = IntStream.rangeClosed(0, 6)
+                .mapToObj(i -> topLeft.add(i, i))
+                .collect(Collectors.toList());
+
+        Point2D botLeft = new Point2D(column - 3, row + 3);
+        List<Point2D> diagonal2 = IntStream.rangeClosed(0, 6)
+                .mapToObj(i -> botLeft.add(i, -i))
+                .collect(Collectors.toList());
+
+        return checkRange(vertical) || checkRange(horizontal)
+                || checkRange(diagonal1) || checkRange(diagonal2);
+    }
+
+    private boolean checkRange(List<Point2D> points) {
+        int chain = 0;
+
+        for (Point2D p : points) {
+            int column = (int) p.getX();
+            int row = (int) p.getY();
+
+            Disc disc = getDisc(column, row).orElse(new Disc(!redMove));
+            if (disc.red == redMove) {
+                chain++;
+                if (chain == 4) {
+                    return true;
+                }
+            } else {
+                chain = 0;
+            }
+        }
+
+        return false;
+    }
+
+    private void gameOver() {
+        System.out.println("Winner: " + (redMove ? "RED" : "YELLOW"));
+    }
+
+    private Optional<Disc> getDisc(int column, int row) {
+        if (column < 0 || column >= COLUMNS
+                || row < 0 || row >= ROWS)
+            return Optional.empty();
+
+        return Optional.ofNullable(grid[column][row]);
+    }
+
+    private static class Disc extends Circle {
+        private final boolean red;
+        public Disc(boolean red) {
+            super(TILE_SIZE / 2, red ? Color.RED : Color.YELLOW);
+            this.red = red;
+
+            setCenterX(TILE_SIZE / 2);
+            setCenterY(TILE_SIZE / 2);
+        }
     }
 
     @Override
@@ -87,7 +199,9 @@ public class Connect4App extends Application {
         stage.setScene(new Scene(createContent()));
         stage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
+
     }
 }
